63d2e9cf9cb8d21b83bd1972dec76996a190d76c
vrnxv <veroniahya02@gmail.com>
2023-05-11 22:11:15 +0700

    manipulasi H2

---
+++ php_pertama.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatibl" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+</head>
+<body>
+    <?php
+    for ($=0; $i<100; $i++) {
+    ?>
+    <h2> Hello World</h2>
+    <?php } ?>
+</body>
+</html>
